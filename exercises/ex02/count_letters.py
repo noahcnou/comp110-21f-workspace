6a84fb7715d964c38243be17d2cc9d1d0a05f3a6
Noah Nouchi <noahcnou@email.unc.edu>
2021-09-08 23:34:25 -0400

    ex02 pt4

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -5,7 +5,7 @@ __author__ = "730408365"
 word = input("Enter a word: ")
 letter = input("What letter do you want to search for? ")
 count = 0
-while len(word) != 1:
+while len(word) != 0:
     if letter == word[0]:
         count = count + 1
     word = word[1:]
