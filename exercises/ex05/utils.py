8b11df1bb1edb7bd26d79cb96bd49ffd1aa44b8d
Noah Nouchi <noahcnou@email.unc.edu>
2021-10-06 23:47:35 -0400

    done

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,54 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730408365"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    a: int = 0
+    b: int = 0
+    if len(xs) == 0:
+        return(xs)
+    while a <= len(xs):
+        if xs[b] % 2 != 0:
+            xs.pop(b)
+        else:
+            b += 1
+            a += 1
+        while a == len(xs):
+            return xs
+    return xs
+
+
+def sub(xs: list[int], x: int, y: int) -> list[int]:
+    new: list[int] = []
+    if x < 0:
+        x = 0
+    if y > len(xs):
+        y = len(xs)
+    if len(xs) == 0:
+        return new
+    while x < y:
+        new.append(int(xs[x]))
+        x += 1
+    return new
+
+
+def concat(xs: list[int], vs: list[int]) -> list[int]:
+    new: list[int] = []
+    while len(xs) + len(vs) == 0:
+        return []
+    while len(xs) == 0:
+        return vs
+    while len(vs) == 0:
+        return xs
+    i: int = 0
+    while i < len(xs):
+        new.append(int(xs[i]))
+        i += 1
+    c: int = 0
+    while c < len(vs):
+        new.append(int(vs[c]))
+        c += 1
+    return new
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,56 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730408365"
+
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens() -> None:
+    xs: list[int] = [9, 11, 13, 22, 24, 2]
+    assert only_evens(xs) == [22, 24, 2]
+
+
+def test_only_evens_with_odd_numbers() -> None:
+    xs: list[int] = [1, 3, 5]
+    assert only_evens(xs) == []
+
+
+def test_sub_empty_list() -> None:
+    xs: list[int] = []
+    a: int = 0
+    b: int = 0
+    assert sub(xs, a, b) == []
+
+
+def test_sub_negative_and_large_indices() -> None:
+    xs: list[int] = [1, 12, 33, 42, 51, 62]
+    a: int = 0
+    b: int = 6
+    assert sub(xs, a, b) == [1, 12, 33, 42, 51, 62]
+
+
+def test_sub_lots_of_items() -> None:
+    xs: list[int] = [1, 5, 8, 52, 74, 77, 92, 104, 111, 500, 1000]
+    a: int = 0
+    b: int = 10
+    assert sub(xs, a, b) == [1, 5, 8, 52, 74, 77, 92, 104, 111, 500]
+
+
+def test_concat_two_empty_lists() -> None:
+    xs: list[int] = []
+    vs: list[int] = []
+    assert concat(xs, vs) == []
+
+
+def test_concat_two_lists_combined() -> None:
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    vs: list[int] = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    assert concat(xs, vs) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+def test_concat_one_empty_lists() -> None:
+    xs: list[int] = []
+    vs: list[int] = [110]
+    assert concat(xs, vs) == [110]
