e6b04b3757eeb0e4a2d1612ef4a136924d26dc38
Noah Nouchi <noahcnou@email.unc.edu>
2021-10-20 14:52:59 -0400

    d

--- exercises/ex06/dictionaries.py
+++
@@ -1,5 +0,0 @@
-"""Practice with dictionaries."""
-
-__author__ = "123456789"
-
-# Define your functions below
--- exercises/ex06/dictionaries_test.py
+++
@@ -1,6 +0,0 @@
-"""Unit tests for dictionary functions."""
-
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
-
-__author__ = "123456789"
