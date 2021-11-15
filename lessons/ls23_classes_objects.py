18d4b4468839b632a18bbfaecabaedeaf6f7b9b9
Noah Nouchi <noahcnou@email.unc.edu>
2021-11-15 02:23:37 -0500

    ls23

--- lessons/ls23_classes_objects.py
+++ lessons/ls23_classes_objects.py
@@ -7,29 +7,38 @@ class Pizza:
     # Attribute definitions
     size: str
     toppings: int
-    extra_cheese: bool
+    extra_cheese: bool = False
 
-    def price(self) -> float:
+    def __init__(self, size: str, toppings: int):
+        """Constructor definition for initialization of attributes."""
+        self.size = size
+        self.toppings = toppings
+    
+    def price(self, tax: float) -> float:
         """Calculate price of a Pizza."""
         total: float = 0.0
         if self.size == "large":
             total += 10.0
         else:
             total += 8.0
-        
+            
         total += self.toppings * 0.75
-        
+            
         if self.extra_cheese:
             total += 1.0
+            
+        total *= tax
         
         return total
 
 
-a_pizza: Pizza = Pizza()
-a_pizza.size = "large"
-a_pizza.toppings = 3
-a_pizza.extra_cheese = False
+a_pizza: Pizza = Pizza("large", 3)
 print(Pizza)
 print(a_pizza)
 print(a_pizza.size)
-print(f"Price: ${a_pizza.price()}")
+print(f"Price: ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+print(another_pizza.size)
+print(f"Price: ${another_pizza.price(1.05)}")
