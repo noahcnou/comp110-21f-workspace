b366833ec4c8c38ea13545e8232041394aad367c
Noah Nouchi <noahcnou@email.unc.edu>
2021-11-30 20:07:03 -0500

    ls28 done

---
+++ lessons/ls28_linked_list.py
@@ -0,0 +1,30 @@
+"""Simple linked list example."""
+
+from __future__ import annotations
+
+from typing import Optional
+
+
+class Node:
+    """Single linked list node."""
+    data: int
+    next: Optional[Node]
+
+    def __init__(self, data: int, next: Optional[Node]):
+        """Constructor."""
+        self.data = data
+        self.next = next
+
+
+def count(head: Optional[Node]) -> int:
+    """Counts up the length of a linked list."""
+    if head is None:
+        return 0
+    else:
+        return 1 + count(head.next)
+
+
+third_node: Node = Node(3, None)
+second_node: Node = Node(2, third_node)
+a_node: Node = Node(1, second_node)
+print(count(a_node))
